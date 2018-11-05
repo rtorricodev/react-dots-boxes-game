0fbe5a6a7a28b1181f91babcedfce393d357d6b5
Rodrigo Torrico Del Castillo <rodrigotorricodelcastillo@Rodrigos-MacBook-Air.local>
2018-11-05 10:02:22 -0400

    Updates

---
+++ precache-manifest.47afcccf36e36e7be16bb13a82bac79b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c7be125459fe7624099",
+    "url": "/myapp/static/css/main.01d81f5b.chunk.css"
+  },
+  {
+    "revision": "9c7be125459fe7624099",
+    "url": "/myapp/static/js/main.9c7be125.chunk.js"
+  },
+  {
+    "revision": "83051ab030ac71ef3d56",
+    "url": "/myapp/static/js/1.83051ab0.chunk.js"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "58b38e58b826072910aaf3d9043a86c7",
+    "url": "/myapp/index.html"
+  }
+];
